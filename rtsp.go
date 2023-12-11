81e5368d7bc0fbd1a2b4704608d36929bd21f686
Brett Jia <dev.bjia56@gmail.com>
2023-12-11 10:16:00 -0500

    sort headers

--- rtsp.go
+++ rtsp.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -147,7 +148,13 @@ type Request struct {
 
 func (r Request) String() string {
 	s := fmt.Sprintf("%s %s %s/%d.%d\r\n", r.Method, r.URL, r.Proto, r.ProtoMajor, r.ProtoMinor)
-	for k, v := range r.Header {
+	keys := make([]string)
+	for k := range r.Header {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for k := range keys {
+		v := r.Header[k]
 		for _, v := range v {
 			s += fmt.Sprintf("%s: %s\r\n", k, v)
 		}
